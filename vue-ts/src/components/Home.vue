aa9239a79e9be93bf358706a9c585d1dbbb6d716
RtyXmd <573850993@qq.com>
2020-12-30 18:20:42 +0800

    init vue-ts

---
+++ vue-ts/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{title}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      title:'Home title'
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
